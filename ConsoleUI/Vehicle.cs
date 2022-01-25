7ae04774d9e013dccdbd93f7bbbf5d85006b25cb
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-25 15:19:34 -0600

    done

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ConsoleUI
+{
+
+
+    public abstract class Vehicle
+
+    {
+        public int Year { get; set; }
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+
+        public abstract void DriveAbstract();
+
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"This {GetType().Name} is virtual in drive ");
+
+
+        }
+
+    }
+}
